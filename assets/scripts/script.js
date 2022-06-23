1915a15971ac5d4fd054582539f1d8edfed97b0c
Andy G <andyguttridge123@gmail.com>
2022-06-23 14:49:25 +0000

    Feature: add event listeners to buttons and stub functions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,22 @@
-console.log('Script linked to html');
+document.addEventListener('DOMContentLoaded', addEventListenersToButtons) 
+
+function addEventListenersToButtons() {
+  let buttons = document.getElementsByTagName('button');
+
+  for (let button of buttons) {
+    if (button.getAttribute('id') === 'start-button') {
+      button.addEventListener('click', playGame)
+    } else if (button.getAttribute('id') === 'info-button') {
+    button.addEventListener('click', displayInstructions)
+    }
+  }
+
+}
+
+function playGame(event) {
+  console.log('Entered playGame()')
+}
+
+function displayInstructions(event) {
+  console.log('Entered displayInstructions()');
+}
